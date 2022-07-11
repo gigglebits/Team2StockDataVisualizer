a111222e9999cb1f685d47d72b9b90fe8e6b7101
Gentry <Trevor.Gentry@t-mobile.com>
2022-07-11 17:59:24 -0500

    Incremental Save before meeting

---
+++ DataFilter.py
@@ -0,0 +1,13 @@
+#The dateFilter class has a dateFilter method. This method will need to take a dictionary (data) and return a modified dictionary that excludes values outside the user's choices
+#Start date and end date will also be in the dictionary format. Example below
+
+#dictionary example 
+    #startDate = {
+        # "year" : 2007
+        # "month" : 11
+        # "day" : 12
+        # }
+
+class dateFilter:
+    def dateFilter(data, startDate, endDate):
+        #data is passed to the date filter method in a dictionary.
